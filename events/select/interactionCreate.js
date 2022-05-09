e41178013cd824cb6948d1c650fb43bc1a5ca332
Resoki <remyaubert71@gmail.com>
2022-05-09 16:57:46 +0200

    first push

--- events/select/interactionCreate.js
+++ events/select/interactionCreate.js
@@ -5,7 +5,7 @@ module.exports = {
      * @param {CommandInteraction} interaction 
      * @param {Client} client 
      */
-    async execute(interaction) {
+    async execute(interaction, client) {
         try {
             if(interaction.isButton()) return;
             if(interaction.customId === 'select-rockstar') {
@@ -19,6 +19,12 @@ module.exports = {
         }
         catch(err) {
             return interaction.reply(`Error:\n${err}`)
+        }
+
+        try {
+            command.run(client, interaction, args)
+        } catch (e) {
+            interaction.reply({ content: e.message });
         }
     }
 }
