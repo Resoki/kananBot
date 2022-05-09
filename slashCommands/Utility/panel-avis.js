0929c3ccaa8c7ab1bec44b46f69cff556833fa06
Resoki <remyaubert71@gmail.com>
2022-05-09 16:14:16 +0200

    first push

---
+++ slashCommands/Utility/panel-avis.js
@@ -0,0 +1,27 @@
+
+module.exports = {
+    name: "panel-avis",
+    aliases: ["avs"],
+    category: "Utility",
+    description: "Show pannel avis !",
+    ownerOnly: false,
+    run: async (client, interaction, args) => {
+      try {
+        const avisEmbed = new client.discord.MessageEmbed()
+        .setTitle(`Avis`)
+        .setDescription(`Tu peux créer un avis via la commande /avis`)
+        .setColor('ORANGE')
+        .setTimestamp()
+        .setThumbnail(client.user.displayAvatarURL({size: 512, dynamic: true}))
+        .setFooter({ text: `Create avis`, iconURL: `${client.user.displayAvatarURL()}` });
+
+        await db.add(`nbAvis`, 1);
+  
+        await interaction.reply({ embeds: [avisEmbed]});
+      }
+      catch(err){
+        return interaction.channel.send(`Une erreur a eu lieu **avis.js** \n${err}`)
+      }
+    },
+  };
+  
