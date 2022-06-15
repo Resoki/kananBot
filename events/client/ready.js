526bc73d24e7b94ee52a2d0801ad0e0d70bc17a4
Resoki <remyaubert71@gmail.com>
2022-06-15 12:59:31 +0200

    make i better

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,6 +1,8 @@
+const cron = require('node-cron');
+const global = require('../../config');
+
 module.exports = {
     name: 'ready',
-
     /**
      * @param {Client} client 
      */
@@ -11,6 +13,14 @@ module.exports = {
             type: "WATCHING",
             name: "KiradUnlocks"
         });
+
+        cron.schedule('* * * * *', async() => {
+            const guild = client.guilds.cache.get(global.guild_id);
+            var memberCount = guild.memberCount;
+
+            const channel = client.channels.cache.find(channel => channel.id === '983316997779632153');
+            await channel.setName(`Membres: ${memberCount}`)
+          });
         
         // Send a message on the console
         console.log(`[LOG] ${client.user.tag} is now online!\n[LOG] Bot serving on Ready to serve in ${client.guilds.cache.size} servers\n[LOG] Bot serving ${client.users.cache.size} users`);
