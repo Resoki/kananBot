5438adf9c436057082f5d917c8d49dc10d2f74f1
Resoki <remyaubert71@gmail.com>
2022-06-15 13:56:27 +0200

    make i better

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     botClientID: "986580340674031636", // client application id du bot (trouveable sur https://discord.com/developers/application)
     botPrefix: "!",
-    channelJoin:"971029374067413013",
+    channelJoin:"986600018268004434",
     guildID: "983043373789233222", // id server
     ownerID: "768393722554613791", // id du proprietaire du serveur
     embedColor: "#00dbff",
